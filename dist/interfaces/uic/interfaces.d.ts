98a65ae6466230e410e4da69d7d59b5c220434cf
Vyacheslav Mishin <v.mishin@lantan.info>
2024-04-11 21:53:11 +0700

    package.json export path aliases were shortened

--- dist/interfaces/uic/interfaces.d.ts
+++
@@ -1,7 +0,0 @@
-/*********************************************************************************************************************/
-export type ValueTransformer<S, T> = (value: S) => T;
-export type IterableConstructor<S, T> = new (iterable: Iterable<S>) => T;
-/*********************************************************************************************************************/
-export type SingleToPair<S, K, V> = ValueTransformer<S, [K, V]>;
-export type SingleToSingle<S, T> = ValueTransformer<S, T>;
-export type PairToSingle<K, V, S> = ValueTransformer<[K, V], S>;
