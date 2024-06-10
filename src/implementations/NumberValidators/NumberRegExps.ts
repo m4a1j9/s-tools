3bea77c1033998ea4cd5b267984df7736cbf8d75
Vyacheslav Mishin <vuacheslav.mishin@mail.ru>
2024-06-10 10:18:29 +0700

    redundant string removal

--- src/implementations/NumberValidators/NumberRegExps.ts
+++ src/implementations/NumberValidators/NumberRegExps.ts
@@ -7,5 +7,4 @@ export const floatNumberRegex = /^(([+-]?(0(?!\d)|[1-9]\d*))\.?\d*)$/gi;
 export const unsignedFloatNumberRegex = /^((0(?!\d)|[1-9]\d*)\.?\d*)$/gi;
 export const negativeFloatingPointNumberRegex = /^(-(0(?!\d)|[1-9]\d*)\.?\d*)$/gi;
 
-
 export const exponentialNumberFormatRegex = /^((([+-]?(0(?!\d)|[1-9]\d*))\.?\d*)([eE])(-?([1-9]\d*)|0))$/gi;
